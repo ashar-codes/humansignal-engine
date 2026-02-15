cc875950d83fbefe6f7246728ea1b328a8671255
ashar-codes <muhammadashar0300@gmail.com>
2026-02-16 02:53:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from core.signal_extractor import extract_signals
+from core.scoring_engine import compute_scores
+
+
+if __name__ == "__main__":
+
+    conversation = """
+    A: You never listen to me.
+    B: That’s not true.
+    A: You always ignore how I feel.
+    B: You’re overreacting again.
+    """
+
+    raw_signals = extract_signals(conversation)
+    result = compute_scores(raw_signals)
+
+    print(result)
 
